ca0ddfc3465788e382045cff3fc0bf4049900afe
jasontyu <jasonyu6154@gmail.com>
2022-08-01 17:47:06 -0700

    Move window.matchMedia mock hack into global test setup

--- components/CalculationCard.test.tsx
+++ components/CalculationCard.test.tsx
@@ -4,21 +4,6 @@ import userEvent from '@testing-library/user-event'
 import { CalculationCard } from './CalculationCard'
 import { Form, InputNumber } from 'antd'
 
-// https://jestjs.io/docs/manual-mocks#mocking-methods-which-are-not-implemented-in-jsdom
-Object.defineProperty(window, 'matchMedia', {
-  writable: true,
-  value: jest.fn().mockImplementation(query => ({
-    matches: false,
-    media: query,
-    onchange: null,
-    addListener: jest.fn(), // deprecated
-    removeListener: jest.fn(), // deprecated
-    addEventListener: jest.fn(),
-    removeEventListener: jest.fn(),
-    dispatchEvent: jest.fn(),
-  })),
-});
-
 describe('CalculationCard', () => {
   let props: React.ComponentProps<typeof CalculationCard>
   let onSubmit: jest.Mock
