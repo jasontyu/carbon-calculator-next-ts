7366875ac1c04d673e38977b5878a40e614cbcb7
jasontyu <jasonyu6154@gmail.com>
2022-08-03 00:26:41 -0700

    Add unit tests for Calculate API

---
+++ components/pages/api/calculate.test.tsx
@@ -0,0 +1,79 @@
+import { createMocks } from 'node-mocks-http'
+import type { NextApiRequest, NextApiResponse } from 'next'
+
+import handler from '../../../pages/api/calculate'
+import * as EmissionsCalculator from '../../../lib/services/EmissionsCalculator'
+
+describe('calculate API', () => {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  const getDataFromResponse = (res: any) => JSON.parse(res._getData())
+
+  let calculateSpy: jest.SpyInstance
+  beforeEach(() => {
+    calculateSpy = jest.spyOn(EmissionsCalculator, 'calculate')
+      .mockImplementation(() => ({ emissions: 42 }))
+  })
+  afterEach(() => {
+    jest.restoreAllMocks()
+  })
+
+  describe('validation', () => {
+    const zodCases = [
+      { title: 'POST with no body', body: undefined },
+      { title: 'POST with empty body', body: {} },
+      { title: 'unrecognized calculationType', body: { calculations: { beverage: { water: 400 } } } },
+      { title: 'recognized calculationType but missing required param', body: { calculations: { food: { bread: 400 } } } },
+      { title: 'recognized calculationType but not wrapped in calculations', body: { food: { bread: 400, meat: 400, vegetables: 400 } } },
+    ]
+    zodCases.forEach(({ title, body }) => it(`400 when ${title}`, async () => {
+      const { req, res }: { req: NextApiRequest, res: NextApiResponse } = createMocks({ method: 'POST', body })
+      await handler(req, res)
+
+      expect(res.statusCode).toBe(400)
+    }))
+
+    it('405 when method !== POST', async () => {
+      const { req, res }: { req: NextApiRequest, res: NextApiResponse } = createMocks({ method: 'GET' })
+      await handler(req, res)
+
+      expect(res.statusCode).toBe(405)
+    })
+  })
+
+  it('200 for single calculation type', async () => {
+    const body = {
+      calculations: {
+        food: { bread: 0, meat: 0, vegetables: 0 }
+      }
+    }
+    const { req, res } = createMocks({ method: 'POST', body })
+    await handler(req, res)
+
+    expect(res.statusCode).toBe(200)
+    expect(calculateSpy).toHaveBeenCalledTimes(1)
+    expect(calculateSpy).toHaveBeenCalledWith({ ctype: 'food', ...body.calculations.food })
+    expect(getDataFromResponse(res)).toStrictEqual({ calculation: {
+      food: { emissions: 42 }
+    } })
+  })
+
+  it('200 for batched calculation types', async () => {
+    const body = {
+      calculations: {
+        food: { bread: 0, meat: 0, vegetables: 0 },
+        transportation: { bus: 0, car: 0, plane: 0 }
+      }
+    }
+    const { req, res }: { req: NextApiRequest, res: NextApiResponse } = createMocks({ method: 'POST', body })
+    await handler(req, res)
+
+    expect(res.statusCode).toBe(200)
+    expect(calculateSpy).toHaveBeenCalledTimes(2)
+    expect(calculateSpy).toHaveBeenCalledWith({ ctype: 'food', ...body.calculations.food })
+    expect(calculateSpy).toHaveBeenCalledWith({ ctype: 'transportation', ...body.calculations.transportation })
+    expect(getDataFromResponse(res)).toStrictEqual({ calculation: {
+      food: { emissions: 42 },
+      transportation: { emissions: 42 }
+    } })
+  })
+})
